f15ee73228843f4d74f4fb034459000ad0915084
apsrinithi <apsrinithi@gmail.com>
2026-02-24 10:34:19 +0530

    Added

---
+++ UC4OOPSBannerApp.java
@@ -0,0 +1,18 @@
+public class UC4OOPSBannerApp {
+    public static void main(String[] args) {
+
+        String[] bannerLines = {
+            String.join("", " ***  ", " ***  ", "**** ", " *** "),
+            String.join("", "*   * ", "*   * ", "*   * ", "*    "),
+            String.join("", "*   * ", "*   * ", "*   * ", "*    "),
+            String.join("", "*   * ", "*   * ", "**** ", " *** "),
+            String.join("", "*   * ", "*   * ", "*    ", "    *"),
+            String.join("", "*   * ", "*   * ", "*    ", "    *"),
+            String.join("", " ***  ", " ***  ", "*    ", " *** ")
+        };
+
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
